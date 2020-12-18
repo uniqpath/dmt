066d7aef4a4e3ac4254af136723a09a512f6a905
David Krmpotic <david.krmpotic@gmail.com>
2020-12-18 22:19:42 +0100

    sample user core

---
+++ etc/sample_user_core/core/node/index.js
@@ -0,0 +1,13 @@
+// Example imports of core DMT functionality:
+//
+// import dmt from 'dmt/bridge';
+// import notify from 'dmt/notify';
+
+// import connectome from 'dmt/connectome';
+// import stores from 'dmt/connectome-stores';
+
+function init(program) {
+  // ...
+}
+
+export default { init };
